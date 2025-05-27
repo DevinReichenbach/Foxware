c80e809e5125317ee772394edef5d2fff2df5804
Devin <dr2676@ship.edu>
2025-05-27 19:57:58 -0400

    Flip orientation of screen to point IR Transmitter outwards

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -3,7 +3,7 @@
 void configureM5() {
   auto cfg = M5.config();
   StickCP2.begin(cfg);
-  StickCP2.Display.setRotation(1);
+  StickCP2.Display.setRotation(3);
   StickCP2.Display.setTextColor(ORANGE);
   StickCP2.Display.setTextDatum(middle_center);
   StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
